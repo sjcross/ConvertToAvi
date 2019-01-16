3e190c83f2db30276f84222973da6fe36cfd5a92
Stephen Cross <stephencross86@gmail.com>
2019-01-16 15:12:44 +0000

    Added avi reading

--- convertToAvi.m
+++ convertToAvi.m
@@ -1,8 +1,13 @@
+% Example function call
+% convertToAvi([pwd,'\'],'Movie 7. Visualising CM-EVs Associating with the Pericardial Wall.avi','MPEG-4',24);
+
 function convertToAvi(pName,fName,format,framerate)
 trueColor = false;
 
 % Need to load image stack from filenames
+[~,~,ext] = fileparts(fName);
 
+if strcmpi(ext,'.tif')
     info = imfinfo(strcat([pName,fName]));
     
     if strfind(info(1).ColorType,'truecolor') == 1
@@ -16,31 +21,40 @@ trueColor = false;
             im(:,:,i) = imread(strcat([pName,fName]),i);
         end
     end
+    
+elseif strcmpi(ext,'.avi')
+    trueColor = true;
+    v = VideoReader([pName,fName]);
+    
+    i=1;
+    while v.hasFrame
+       im(:,:,:,i) = readFrame(v);
+       i = i+1;
+    end    
+end
 
 [~,fName,~] = fileparts(fName);
 
 if nargin >= 4
-   fName = strcat([fName,'_',num2str(framerate),'fps']); 
+    fName = strcat([fName,'_',num2str(framerate),'fps']);
 end
 
 if numel(findstr(format,'MPEG-4')) ~= 0
     v = VideoWriter(strcat([pName,fName,'.mp4']),'MPEG-4');
-    
+    v.Quality = 100;
 elseif numel(findstr(format,'Motion JPEG AVI')) ~= 0
     v = VideoWriter(strcat([pName,fName,'.avi']),'Motion JPEG AVI');
-    elseif numel(findstr(format,'ARCHIVAL')) ~= 0
+elseif numel(findstr(format,'ARCHIVAL')) ~= 0
     v = VideoWriter(strcat([pName,fName]),'ARCHIVAL');
-        elseif numel(findstr(format,'Uncompressed AVI')) ~= 0
+elseif numel(findstr(format,'Uncompressed AVI')) ~= 0
     v = VideoWriter(strcat([pName,fName]),'Uncompressed AVI');
-            elseif numel(findstr(format,'MOTION JPEG 2000')) ~= 0
+elseif numel(findstr(format,'MOTION JPEG 2000')) ~= 0
     v = VideoWriter(strcat([pName,fName]),'MOTION JPEG 2000');
 end
 
 if nargin >= 4
-   v.FrameRate = framerate; 
+    v.FrameRate = framerate;
 end
-
-v.Quality = 100;
 
 open(v);
 
